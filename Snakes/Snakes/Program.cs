5a9131035be95be437797650808980c75b858da8
Vitny <thevitny@gmail.com>
2020-04-02 22:45:02 +0300

    Линии

--- Snakes/Snakes/Program.cs
+++ Snakes/Snakes/Program.cs
@@ -10,27 +10,11 @@ namespace Snakes
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(6,6,'o');
-            Point p2 = new Point(8,4,'o');
-            Point p3 = new Point(2, 7, 'o');
-            Point p4 = new Point(3, 6, 'o');
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-            
-            foreach(Point i in pList)
-            {
-                i.Draw();
-            }
-            Console.ReadLine();
+            HorizontalLine line1 = new HorizontalLine(3, 7, 5, 'o');
+            VerticalLine line2 = new VerticalLine(11, 18, 8, 'o');
+            line1.DrawHor();
+            line2.DrawVert();
+            Console.ReadLine(); 
         }
        
     }
